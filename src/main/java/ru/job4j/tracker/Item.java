4a2e46609142db371e3230b136e1b64ec190bf1a
Evgeny Chemagin <eugenechev@gmail.com>
2021-12-01 20:56:32 +0300

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,13 @@
 package ru.job4j.tracker;
 
+import java.time.*;
+
+import static java.time.LocalDateTime.now;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = now();
 
     public Item() {
     }
@@ -30,5 +35,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
